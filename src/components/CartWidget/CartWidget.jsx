f58fef46835e7c34bfa7747cc677828e441eaa26
Franco Gatti <francogatti.sdp@outlook.com>
2024-08-28 16:28:30 -0300

    first commit

---
+++ src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,10 @@
+import './CartWidget.css'
+const CartWidget = () => {
+  return (
+    <div>
+        <img className='CartImg' src="src\Components\CartWidget\cart-shopping-svgrepo-com.svg" alt="Carrito de Compra" />
+    </div>
+  )
+}
+
+export default CartWidget
